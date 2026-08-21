d46082612ac5bc7aa654c1666ebd456628834a97
lancelouan <acelouan@gmail.com>
2026-08-21 18:03:46 +0200

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'planning-revision-v1';
+const FILES_TO_CACHE = [
+  '/Planning-Revision/',
+  '/Planning-Revision/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => cachedResponse || fetch(event.request))
+  );
+});
